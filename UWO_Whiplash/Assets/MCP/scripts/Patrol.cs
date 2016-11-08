d4649bb193cb7c838fe89355bd4bbc6bc34b6aba
TYS-PC\Tyler <tdavis1991@live.ca>
2016-11-08 12:31:00 -0500

    Initial Commit
    Contains Oculus Utilities and VR Support

---
+++ UWO_Whiplash/Assets/MCP/scripts/Patrol.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Patrol : MonoBehaviour {
+
+    float accel = 0.25f;
+    float decel = 0.09f;
+    float maxSpeed = 8.0f;
+    float currentSpeed = 0.0f;
+    float minSpeed = 2.0f;
+
+    int stopOrGo= 1;
+    int WPIndex = 0;
+    float slowRotate = 1;
+
+    public Transform[] waypoints;
+    Transform currentWaypoint;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stopOrGo == 1)
+        {
+            GO();
+        }
+        else if (stopOrGo == 0)
+        {
+            STOP();
+            
+        }
+        currentWaypoint = waypoints[WPIndex];
+	}//END Update
+
+    void GO()
+    {
+        if(WPIndex == 2 || WPIndex ==6 || WPIndex ==11 || WPIndex ==15)
+        {
+            stopOrGo = 0;
+        }
+        if (currentWaypoint)
+        {
+            var rotation = Quaternion.LookRotation(currentWaypoint.position - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * slowRotate);
+        }
+
+        currentSpeed = currentSpeed + accel * accel;
+        transform.Translate(0, 0, Time.deltaTime * currentSpeed);
+
+        if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+    }
+
+   void STOP()
+    {
+        if (currentWaypoint)
+        {
+            var rotation = Quaternion.LookRotation(currentWaypoint.position - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * slowRotate);
+        }
+
+        currentSpeed = currentSpeed - decel;
+        transform.Translate(0, 0, Time.deltaTime * currentSpeed);
+
+        if (currentSpeed < minSpeed)
+        {
+            currentSpeed = minSpeed;
+        }
+
+        if(WPIndex ==0 || WPIndex ==4 || WPIndex ==9 || WPIndex == 13)
+        {
+            stopOrGo = 1;
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        WPIndex++;
+
+        if(WPIndex > waypoints.Length)
+        {
+            WPIndex = 0;
+            currentWaypoint = waypoints[WPIndex];
+        }
+    }
+}
---
+++ UWO_Whiplash/Assets/Path01.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.Threading;
+
+public class Path01 : MonoBehaviour {
+
+    float accel = 0.25f;
+    float decel = 0.09f;
+    float maxSpeed;
+    float minSpeed = 2.0f;
+    private float currentSpeed = 1.0f;   
+   float rotationDamping = 1f;//Makes rotation during turns slower rather than immediate. 
+    Stopwatch redTimer = new Stopwatch();
+
+    private int stopOrGo = 1; //stop is 0, Go is 1;
+    private int WPIndexPointer = 0;
+    
+    bool smoothRotation = true;
+
+    
+    private Transform currentWaypoint;
+    //holds the current waypoint
+
+    public Transform[] waypoints;
+    //holds the waypoints assigned in the inspector
+
+    //public GameObject streetLight;
+    //traffic light to check for red
+    
+
+    void Start()
+    {
+        
+        stopOrGo = 1;
+        
+    }//END start
+
+    void Update()
+    {
+        
+        if (stopOrGo == 1)
+        {
+            //function that will set the speed and rotation speed according to certain waypoints.
+            //StartCoroutine(GO());
+            GO();
+            
+        }
+        if (stopOrGo == 0)
+        {
+            STOP();
+        }
+        
+        currentWaypoint = waypoints[WPIndexPointer];
+    }//END update
+
+    void GO()
+    {
+       
+        if (WPIndexPointer == 1){
+            maxSpeed = 2.0f;            
+        }
+           
+        if (WPIndexPointer == 7)
+        {     maxSpeed = 2.5f;       
+                       
+        }
+        if(WPIndexPointer == 9)
+        {
+            maxSpeed = 10.0f;
+            
+        }
+        if (WPIndexPointer == 11)
+        {
+            stopOrGo = 0;
+        }
+        if (WPIndexPointer == 12)
+        {
+            maxSpeed = 3.0f;
+        }
+        
+        if (WPIndexPointer == 14)
+        {
+            maxSpeed = 8.0f;
+        }
+        if (WPIndexPointer == 15)
+        {
+            // MeshRenderer streetLightRenderer = streetLight.GetComponent<MeshRenderer>();
+
+            //  Material streetLightColour = streetLightRenderer.material;
+            // Material red = streetLightRenderer.material;
+
+            //  while (streetLightColour == red)
+            //  {
+            //streetLightRenderer = streetLight.GetComponent<MeshRenderer>();
+            //    streetLightColour = streetLightRenderer.material;                
+            // }
+
+            //decel to street light
+            stopOrGo = 0;
+                        
+        }
+        if (WPIndexPointer == 16)
+        {
+           // redTimer.Start();
+
+
+           // System.TimeSpan redCheck = redTimer.Elapsed;
+                      
+          //  if (redCheck.Seconds <= 3)
+           // {
+              
+               //currentSpeed = 0.0f;
+              //  accel = 0f;
+           // }
+           // else
+           // {
+            //    redTimer.Stop();
+            //    accel = 0.25f;
+          //  }
+
+        }
+        if (WPIndexPointer == 17)
+        {
+            //decel to street light
+            //decel = 1f;
+            stopOrGo = 0;
+        }
+        if (WPIndexPointer == 18)
+        {
+
+            redTimer.Start();
+             System.TimeSpan redCheck = redTimer.Elapsed;
+       
+              if (redCheck.Seconds <= 4)
+             {
+
+            currentSpeed = 0.0f;
+              accel = 0f;
+             }
+             else
+             {
+                redTimer.Stop();
+                accel = 0.25f;
+              }
+
+            maxSpeed = 3.0f;
+        }
+        if(WPIndexPointer==20)
+        {
+            maxSpeed = 7.0f;
+  
+        }
+        if (WPIndexPointer == 24)
+        {
+            maxSpeed = 0f;
+            accel = 0f;
+        }
+
+        if (currentWaypoint)
+        {
+            
+            //Quaternion.LookRotation creates the car's rotation in turns. Function takes Vector3 variables to determine forward movement and upward movement.
+            var rotation = Quaternion.LookRotation(currentWaypoint.position - transform.position);
+
+            //This function simulates a car turning by slowing the rotation speed.
+            //transform.rotation is the car's current rotation.
+            //Quaternion.Slerp measures the rotation between 2 quaternions based on a time parameter.            
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+        }
+        //Accelerate towards waypoint
+        currentSpeed = currentSpeed + accel * accel;
+        transform.Translate(0, 0, Time.deltaTime * currentSpeed);
+
+        if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+
+        
+    }//END GO
+
+    void OnTriggerEnter()
+    {
+        redTimer.Reset();
+
+
+        WPIndexPointer++;
+       
+
+        //if (WPIndexPointer >= waypoints.Length)
+        //{
+         //   WPIndexPointer = 0;
+         
+        //}
+      
+        
+    }
+
+    void STOP()
+    {
+        if (currentWaypoint)
+        {
+
+            //Quaternion.LookRotation creates the car's rotation in turns. Function takes 1 or 2 Vector3 variables to determine forward movement and upward movement.
+            var rotation = Quaternion.LookRotation(currentWaypoint.position - transform.position);
+
+            //This function simulates a car turning by slowing the rotation speed.
+            //transform.rotation is the car's current rotation.
+            //Quaternion.Slerp measures the rotation between 2 quaternions based on a time parameter.            
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+        }
+
+        currentSpeed = currentSpeed - decel;
+        transform.Translate(0,0, Time.deltaTime*currentSpeed);
+
+        if(currentSpeed<= minSpeed)
+        {
+            currentSpeed = minSpeed;
+        }
+
+        
+        if (WPIndexPointer == 12 || WPIndexPointer==16 || WPIndexPointer==18)
+        {
+            stopOrGo = 1;
+        }
+        
+    }//END STOP
+
+    
+}
